7e437773bce0960fd13df7ba1559178cc1e5401c
jeffdiers <jeff.diers@gmail.com>
2019-05-07 18:51:52 -0600

    trying to deploy react

--- api/app.js
+++ api/app.js
@@ -9,14 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+// app.set('view engine', 'html');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/api/', indexRouter);
-app.use('/api/users', usersRouter);
+// app.use('/api/', indexRouter);
+// app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
