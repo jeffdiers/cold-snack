5f3838ed522487a6283038dae17cf9932e5a9c4b
jeffdiers <jeff.diers@gmail.com>
2019-05-07 21:19:06 -0600

    test proxy with deploy

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
