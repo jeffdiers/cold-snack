748ab8d406035ae45b350612d694d238d869d36d
jeffdiers <jeff.diers@gmail.com>
2019-05-07 19:48:26 -0600

    deploy works?

--- api/public/service-worker.js
+++ api/public/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.708501c981f1dfe73f557382f7e6e906.js"
 );
 
 self.addEventListener('message', (event) => {
